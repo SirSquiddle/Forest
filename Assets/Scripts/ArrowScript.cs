c826b26b3ad6df60c6e780a34ed3c2d9ee73cebc
Thomas Passelegue <tpasselegu@univ-lyon2.fr>
2018-11-21 13:35:40 +0100

    Ajout du tir de flèche en appuyant sur SPACE si le personnage a sa variable 'hasBow' à true.

---
+++ Assets/Scripts/ArrowScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour
+{
+
+    public float timer = 0;
+    public string orientation = "";
+    public GameObject character = null;
+    public float speed = 25;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    orientation = GameObject.Find("ForestCharacter").GetComponent<CharacterController>().orientation;
+	    gameObject.transform.position = GameObject.Find("ForestCharacter").transform.position;
+        timer = 0;
+	    switch (orientation)
+	    {
+            case "up":
+	            gameObject.transform.rotation = new Quaternion(0, 0, 0, 0);
+	            break;
+
+	        case "down":
+	            gameObject.transform.rotation = new Quaternion(0, 0, -1, 0);
+                break;
+
+	        case "right":
+	            //TODO right
+	            gameObject.transform.rotation = new Quaternion(0, 0, -0.7f, 0.7f);
+                break;
+
+	        case "left":
+                //TODO left
+                gameObject.transform.rotation = new Quaternion(0, 0, 0.7f, 0.7f);
+                break;
+	    }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    timer += Time.deltaTime;
+        Debug.Log(gameObject.transform.rotation);
+	    switch (orientation)
+	    {
+	        case "up":
+	            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + speed * Time.deltaTime, gameObject.transform.position.z);
+                break;
+
+	        case "down":
+	            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - speed * Time.deltaTime, gameObject.transform.position.z);
+                break;
+
+	        case "right":
+	            gameObject.transform.position = new Vector3(gameObject.transform.position.x + speed * Time.deltaTime, gameObject.transform.position.y, gameObject.transform.position.z);
+                break;
+
+	        case "left":
+	            gameObject.transform.position = new Vector3(gameObject.transform.position.x - speed * Time.deltaTime, gameObject.transform.position.y, gameObject.transform.position.z);
+                break;
+	    }
+
+
+        if (timer >= 5)
+	    {
+            Destroy(gameObject);
+	    }
+	}
+}
--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,27 +1,104 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Policy;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
 
     public float speed = 1;
+    public bool hasBow = false;
+    public string orientation = "down";
+    public GameObject arrow = null;
 
-	// Use this for initialization
-	void Start () {
+    
+
+	void Start ()
+	{
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
         {
+            //Moves the player from Input.GetAxis("Horizontal")*Time.deltaTime on the x axis
+
             gameObject.transform.position = new Vector3(gameObject.transform.position.x + speed*Input.GetAxis("Horizontal")*Time.deltaTime, gameObject.transform.position.y, gameObject.transform.position.z);
+
+            //change orientation according to Input.GetAxisRaw("Horizontal") --- if -1 -> "left" --- if 1 -> "right" 
+
+            if (Input.GetAxisRaw("Horizontal") == -1)
+            {
+                orientation = "left";
+            }
+            else if (Input.GetAxisRaw("Horizontal") == 1)
+            {
+                orientation = "right";
+            }
         }
 
         if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.UpArrow))
         {
+            //Moves the player from Input.GetAxis("Vertical")*Time.deltaTime on the y axis
+
             gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + speed * Input.GetAxis("Vertical")*Time.deltaTime, gameObject.transform.position.z);
+
+            //change orientation according to Input.GetAxisRaw("Vertical") --- if -1 -> "down" --- if 1 -> "up" 
+
+            if (Input.GetAxisRaw("Vertical") == -1)
+            {
+                orientation = "down";
+            }
+            else if (Input.GetAxisRaw("Vertical") == 1)
+            {
+                orientation = "up";
+            }
+        }
+
+	    if (Input.GetKeyDown(KeyCode.Space))
+	    {
+            Shoot();
+	    }
+	}
+
+    public void Shoot()
+    {
+        if (hasBow)
+        {
+            //shoots an arrow in the right direction
+
+            Instantiate(arrow);
+            /*switch (orientation)
+            {
+                case "up":
+                    Debug.Log("*Shoots arrow graciously upward*");
+                    //TODO shoots an arrow upward
+                    Instantiate(arrow);
+                    break;
+
+                case "down":
+                    Debug.Log("*Shoots arrow graciously downward*");
+                    //TODO shoots an arrow downward
+                    Instantiate(arrow);
+                    break;
+
+                case "right":
+                    Debug.Log("*Shoots arrow graciously to the right*");
+                    //TODO shoots an arrow to the right
+                    Instantiate(arrow);
+                    break;
+
+                case "left":
+                    Debug.Log("*Shoots arrow graciously to the left*");
+                    //TODO shoots an arrow to the left
+                    Instantiate(arrow);
+                    break;
+            }*/
+        }
+        else
+        {
+            Debug.Log("You have no bow!");
         }
     }
 }
