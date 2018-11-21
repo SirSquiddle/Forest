0548a2992b2c8a4ccb31d8c4f97dcd1310f0911d
Thomas Passelegue <tpasselegu@univ-lyon2.fr>
2018-11-21 11:14:47 +0100

    First commit

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Hosting;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void QuitGame()
+    {
+        Debug.Log("Je quitte le jeu");
+        Application.Quit();
+    }
+}
